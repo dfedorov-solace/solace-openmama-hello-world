328221ea38af106ae987e6af1c6d5d18536dc2f5
Dmitri Fedorov <dfedorov.solace@gmail.com>
2016-07-13 11:05:46 -0700

    Load Solace middleware bridge and open it
    Signed-off-by: Dmitri Fedorov <dfedorov.solace@gmail.com>

---
+++ topicPublishOne.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016 Solace Systems, Inc. All rights reserved.
+ *
+ * http://www.solacesystems.com
+ *
+ * This source is distributed under the terms and conditions
+ * of any contract or contracts between Solace and you or
+ * your company. If there are no contracts in place use of
+ * this source is not authorized. No support is provided and
+ * no distribution, sharing with others or re-use of this
+ * source is authorized unless specifically stated in the
+ * contracts referred to above.
+ */
+
+/*
+ * Solace messaging with OpenMAMA
+ * PublishSubscribe tutorial - Topic Publisher
+ * Demonstrates publishing one direct message to a topic
+ */
+
+#include <stdio.h>
+#include <mama/mama.h>
+
+int main(int argc, const char** argv)
+{
+    printf("Solace OpenMAMA tutorial.\nPublishing one message with OpenMAMA.\n");
+
+    mama_status status;
+    mamaBridge bridge = NULL;
+    // load Solace middleware bridge and open it
+    if (((status = mama_loadBridge(&bridge, "solace")) == MAMA_STATUS_OK) &&
+        ((status = mama_open()) == MAMA_STATUS_OK))
+    {
+        printf("Closing Solace middleware bridge.\n");
+        mama_close();
+        // normal exit
+        exit(0);
+    }
+    printf("OpenMAMA error: %s\n", mamaStatus_stringForStatus(status));
+    exit(status);
+}
+
